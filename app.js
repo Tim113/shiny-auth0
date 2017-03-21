474b5262eb41407253f3d3a650646b27fbfdd76b
Tim Jones <timothy.jones1@amey.co.uk>
2017-03-21 09:55:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var strategy = new Auth0Strategy({
   }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
+    localStorage.setItem('token', extraParams.id_token); // write
     // profile has all the information from the user
     return done(null, profile);
   });
