285eefc6554fc923a7d1ee2b38a7a7b05103e132
Tim Jones <timothy.jones1@amey.co.uk>
2017-03-21 09:37:11 +0000

    Returned extraParams from Auth0Strategy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var strategy = new Auth0Strategy({
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
-    return done(null, profile);
+    return done(null, profile, extraParams);
   });
 
 passport.use(strategy);
